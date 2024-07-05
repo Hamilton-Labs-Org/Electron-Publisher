da188ee99f0220404a5d2599635fb9bbae5980d3
Hamilton Labs <173300401+hamilton-labs@users.noreply.github.com>
2024-07-05 10:45:07 -0400

    📝 Updated README

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'hamilton-labs',
+          name: 'Electron-Publisher'
+        },
+        prerelease: true,
+        draft: true
+      }
+    }
+  ]
 };
