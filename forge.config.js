0697f1106965583e8801516d67d831a9c96856a7
Hamilton Labs <173300401+hamilton-labs@users.noreply.github.com>
2024-07-30 14:44:20 -0400

    🛠  Added Linux to maker-zip

--- forge.config.js
+++ forge.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin','linux'],
     },
     {
       name: '@electron-forge/maker-deb',
