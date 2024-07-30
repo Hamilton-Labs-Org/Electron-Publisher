cdf186711377820f1c8111c4671dd46858fb8fd7
Hamilton Labs <173300401+hamilton-labs@users.noreply.github.com>
2024-07-30 07:48:44 -0400

    🛠  Reconnected contextBridge

--- dist/preload/index.mjs
+++ dist/preload/index.mjs
@@ -1,17 +1,22 @@
-import require$$0 from "electron";
-var preload = {};
-const { contextBridge, ipcRenderer } = require$$0;
-contextBridge.exposeInMainWorld("versions", {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke("ping")
-  // we can also expose variables, not just functions
-});
-contextBridge.exposeInMainWorld("darkMode", {
-  toggle: () => ipcRenderer.invoke("dark-mode:toggle"),
-  system: () => ipcRenderer.invoke("dark-mode:system")
-});
-export {
-  preload as default
-};
+import { contextBridge, ipcRenderer } from "electron";
+import __cjs_mod__ from "node:module";
+const __filename = import.meta.filename;
+const __dirname = import.meta.dirname;
+const require2 = __cjs_mod__.createRequire(import.meta.url);
+if (process.contextIsolated) {
+  try {
+    await contextBridge.exposeInMainWorld("versions", {
+      node: () => process.versions.node,
+      chrome: () => process.versions.chrome,
+      electron: () => process.versions.electron,
+      ping: () => ipcRenderer.invoke("ping")
+      // we can also expose variables, not just functions
+    });
+    await contextBridge.exposeInMainWorld("darkMode", {
+      toggle: () => ipcRenderer.invoke("dark-mode:toggle"),
+      system: () => ipcRenderer.invoke("dark-mode:system")
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
