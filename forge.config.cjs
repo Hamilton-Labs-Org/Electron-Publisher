181eed03c4dac5fcca779c6eca2c7bb1f9c2ae0a
Hamilton Labs <173300401+hamilton-labs@users.noreply.github.com>
2024-08-13 16:20:11 -0400

    🖼  Fixed Icons

--- forge.config.cjs
+++ forge.config.cjs
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: './assets/images/icon.ico',
     // ignore: [
     //   /^\/src/,
     //   /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/,
@@ -16,7 +17,8 @@ module.exports = {
       config: {
         authors: 'Hamilton Labs',
         name: 'HamiltonLabs',
-        description: 'Electron Publisher Tutorial'
+        description: 'Electron Publisher Tutorial',
+        icon: './assets/images/icon.ico',
       },
     },
     {
@@ -39,11 +41,11 @@ module.exports = {
     },
     new FusesPlugin({
       version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: true,
-      [FuseV1Options.EnableCookieEncryption]: false,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
       [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
